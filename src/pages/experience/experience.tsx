a80001ef385cd09d916a85d07ad8081e4a20290c
Johnson <jdinh782@gmail.com>
2026-04-15 19:24:48 -0700

    update: experience descriptions

--- src/pages/experience/experience.tsx
+++ src/pages/experience/experience.tsx
@@ -15,12 +15,12 @@ export default function Experience() {
 
     const experiences: Array<ExperienceInterface> = [
         {
-            organization: "DFS",
-            href: "https://www.dataforsports.app/",
+            organization: "Polystack Digital",
+            href: "https://www.polystackdigital.com/",
             team: "",
-            position: "Lead Software Engineer",
-            timeline: "August 2023 - Present",
-            description: "Leading a team of software engineers to build a service platform featuring analytics, modeling, and predictions on sports data. Integrating batch processing techniques to enhance peformance on ETL pipelines and reduce data processing time by 30%. Designing a responsive, intuitive UI with modern React frameworks, resulting in a 20% increase in customer satisfaction. Maintaining a near 100% system uptime to ensure seamless access for 100+ active users during peak sports seasons.",
+            position: "Software Engineer",
+            timeline: "Janaury 2026 - Present",
+            description: "Focused on elevating the valuation of high-growth SaaS products by 20% through the architecture of scalable software foundations and a modular SaaS blueprint. I developed high-efficiency digital workflows and custom automation tools that empowered global entrepreneurs with 30% greater operational freedom and accelerated the time-to-market for new ventures by 40%. Additionally, I drove a 15% increase in user retention through the rapid deployment of sleek, high-performance user interfaces and ensured long-term product stability by optimizing infrastructure to maintain a 99.9% service availability standard.",
             technologies: ["Next.js", "React", "TypeScript", "HTML5", "CSS3", "ESLint", "Supabase", "DigitalOcean", "Visual Studio Code", "WebStorm", "Postman", "Docker", "Cloudflare", "GitHub", "Confluence", "Jira"]
         },
         {
@@ -29,7 +29,7 @@ export default function Experience() {
             team: "Parcel Pro",
             position: "Applications Developer",
             timeline: "June 2023 - Present",
-            description: "Leading a team of full-stack developers to design a real-time tracking system that provides accurate, live updates for over 10,000 domestic and international shipments. Engineering software solutions that integrate SQL Server databases to extract shipment data from tracking numbers, presenting it through a responsive user interface built with modern Angular frameworks. Enhancing application performance by minimizing asynchronous issues and optimizing component rendering through modular programming. Collaborating with the Parcel Pro development team, I help launch early application versions, increasing user satisfaction by 30%.",
+            description: "Optimized product market fit and transformed customer satisfaction by 40% by bridging the gap between engineering and support to implement critical user feedback. I spearheaded the full-stack development of flagship products like UPS Wallet and DeliveryDefense while engineering a high-scale visualization system that managed real-time data for over 100,000 domestic and international shipments. To improve operational ROI, I established coding standards that reduced technical debt by 40% and significantly accelerated feature delivery by halving new developer onboarding time.",
             technologies: ["Angular", "HTML5", "CSS3", "TypeScript", "ESLint", "C#", ".NET", "Visual Studio Code", "Visual Studio", "Postman", "Microsoft SQL Server", "Microsoft Azure", "Azure DevOps", "Docker", "Kubernetes", "Confluence", "Jira"]
         },
         {
@@ -38,7 +38,7 @@ export default function Experience() {
             team: "Consumer and Investment Banking",
             position: "Software Engineer Program Intern",
             timeline: "June 2022 - August 2022",
-            description: "Prototyped a web software development kit that retrieves internal and external application metrics, monitors user behavior, and displays product usage for over 100 clients and business partners. Built dashboard visualizations and stories using Tableau to showcase timelines of client productivity and application utilization. Leveraged React and Material UI frameworks to create a visual prototype to display these dashboards within a web application. Operated an experimental version of the kit, enabling clients to generate analytical insights for over 1,000 services, boosting productivity by up to 85%.",
+            description: "Enhanced internal asset valuation and business decision-making by constructing UI prototypes and Tableau dashboards to visualize application usage for over 1,000 distinct services. I upscaled operational productivity to 85% by refining SDK tools and validated product-led growth strategies by prototyping a web SDK specifically designed to track user behavior for more than 100 high-value clients. My role also involved translating technical complexity into actionable strategy for executive partners and strengthening organizational security through data governance standards that reduced data misuse by 30%.",
             technologies: ["React", "Material UI", "HTML5", "CSS3", "TypeScript", "ESLint", "Node.js", "Express.js", "Python", "Visual Studio Code", "Swagger", "Tableau", "Apache Hadoop", "Elastic", "Kibana", "Bitbucket", "Jira"]
         },
         {
@@ -54,9 +54,9 @@ export default function Experience() {
             organization: "University of California, Irvine",
             href: "https://uci.edu/",
             team: "Disability Services Center",
-            position: "Course Notes Developer",
+            position: "Student Course Programmer",
             timeline: "January 2022 - April 2022",
-            description: "Advised mini-lectures and office hours for professors by providing course materials and notes for over 500 students. Covered a range of topics across programming and math courses, including data structures, machine learning, and advanced algebra. Organized and stored more than 50 hand-written and typed course notes in online server solutions, ensuring ease of access for all students.",
+            description: "Increased student career alignment by 25% by architecting personalized academic roadmaps for over 200 individuals. I boosted average course performance by 15% for a population of 500+ students by curating a centralized library of high-impact technical resources and Facilitating weekly tutoring sessions that resulted in a 20% lift in exam scores. Additionally, I modernized the educational offering by overhauling core curriculum modules and accelerated the transition from theory to professional practice by mentoring students in object-oriented design and complex memory logic.",
             technologies: ["Notion", "Microsoft Word", "Microsoft OneNote", "Microsoft Excel", "Microsoft PowerPoint", "Microsoft SharePoint", "Microsoft OneDrive"]
         }
     ];
